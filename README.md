3de13a1830750e5fb558b52904e68d15c5b81f57
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2017-03-01 14:03:29 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# moodle-local_aws
+
+A moodle plugin containing Amazon's SDK for PHP.
+
+To use the sdk, simply include the autoloader contained within this plugin.
+
+<pre>
+require_once($CFG->dirroot . '/local/aws/sdk/aws-autoloader.php');
+</pre>
+
+
---
+++ version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information.
+ *
+ * @package   local_aws
+ * @author    Kenneth Hendricks <kennethhendricks@catalyst-au.net>
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2017030100;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2017030100;      // Same as version.
+$plugin->requires  = 2014051217;
+$plugin->component = "local_aws";
+$plugin->maturity  = MATURITY_STABLE;
+

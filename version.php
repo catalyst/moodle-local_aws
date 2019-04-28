3ef03e42c49040cc1c252d279fba317f54606146
eWallah <info@eWallah.net>
2019-04-28 15:30:09 +0200

    latest sdk version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018061900;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2018061900;      // Same as version.
+$plugin->version   = 2019042800;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2019042800;      // Same as version.
 $plugin->requires  = 2013111811;
 $plugin->component = "local_aws";
 $plugin->maturity  = MATURITY_STABLE;

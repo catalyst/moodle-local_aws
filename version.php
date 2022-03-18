d4f3284b752a35aeac1272236fb870820fdc8542
Peter Burnett <peterburnett@catalyst-au.net>
2022-03-18 16:18:09 +1000

    Version bump for new API function

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022011300;
+$plugin->version   = 2022011301;
 $plugin->release   = '3.208.1'; // This should be in lock step with sdk/CHANGELOG.md.
 $plugin->requires  = 2013111811;
 $plugin->component = 'local_aws';

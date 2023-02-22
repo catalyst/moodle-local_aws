417d50d824035fef970efe96fbc3c969fcb50bb4
Sarah Cotton <sarah.cotton@catalyst-eu.net>
2023-02-22 15:44:20 +0000

    Fix aws library conflicts in Totara

--- sdk/aws-autoloader.php
+++ sdk/aws-autoloader.php
@@ -2826,9 +2826,13 @@ spl_autoload_register(function ($class) use ($mapping) {
     }
 }, true);
 
-require __DIR__ . '/Aws/functions.php';
-require __DIR__ . '/GuzzleHttp/functions_include.php';
-require __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
-require __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
-require __DIR__ . '/JmesPath/JmesPath.php';
-require __DIR__ . '/Symfony/Polyfill/Intl/Idn/bootstrap.php';
+if (!function_exists('Aws\constantly'))  {
+    require_once __DIR__ . '/Aws/functions.php';
+}
+if (!function_exists('GuzzleHttp\describe_type')) {
+    require_once __DIR__ . '/GuzzleHttp/functions_include.php';
+}
+require_once __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
+require_once __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
+require_once __DIR__ . '/JmesPath/JmesPath.php';
+require_once __DIR__ . '/Symfony/Polyfill/Intl/Idn/bootstrap.php';

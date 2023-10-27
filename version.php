9cdf20cd248eee36751346dba0448416ca77e550
Misha Golenkov <mikhailgolenkov@catalyst-au.net>
2023-10-27 14:56:44 +1100

    Bump the plugin version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023072100;
-$plugin->release   = '3.276.2'; // This should be in lock step with sdk/CHANGELOG.md.
-$plugin->requires  = 2013111811;
+$plugin->version   = 2023102700;
+$plugin->release   = '3.283.13'; // This should be in lock step with sdk/CHANGELOG.md.
+$plugin->requires  = 2023042400; // Moodle 4.2
 $plugin->component = 'local_aws';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [26, 402]; // A range of branch numbers of supported moodle versions.
+$plugin->supported = [402, 402]; // A range of branch numbers of supported moodle versions.

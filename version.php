03f3dceb903554cf80732460f2cbfae8942ae28b
Misha Golenkov <mikhailgolenkov@catalyst-au.net>
2024-05-08 12:09:52 +1000

    Bump CI up to Moodle 4.4

--- version.php
+++ version.php
@@ -30,4 +30,4 @@ $plugin->release   = '3.276.2'; // This should be in lock step with sdk/CHANGELO
 $plugin->requires  = 2013111811;
 $plugin->component = 'local_aws';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [26, 402]; // A range of branch numbers of supported moodle versions.
+$plugin->supported = [26, 404]; // A range of branch numbers of supported moodle versions.
